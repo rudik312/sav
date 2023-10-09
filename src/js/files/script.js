767ddc5c704ed327c9865a15fab372ea2baf9d9d
rudik312 <fl.rudik@mail.ru>
2023-10-09 22:44:22 +0700

    update adaptive cases

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -40,7 +40,32 @@ window.onload = function() {
       arrows:false, 
       speed: 1000,
       infinite: true,
-      
+      responsive: [
+        {
+          breakpoint: 950,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+      ]
     });
 
 
