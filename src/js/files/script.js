fa4909f97b57893cb34a5a3b204199fd2b614af7
rudik312 <fl.rudik@mail.ru>
2023-10-10 00:00:07 +0700

    update adaptive cases

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -58,10 +58,17 @@ window.onload = function() {
         {
           breakpoint: 480,
           settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
+            slidesToShow: 2,
+            slidesToScroll: 2,
           }
-        }
+        },
+        {
+          breakpoint: 320,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+          }
+        },
         // You can unslick at a given breakpoint now by adding:
         // settings: "unslick"
         // instead of a settings object
